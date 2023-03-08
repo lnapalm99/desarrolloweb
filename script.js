895b65e0cf82029d189559a937a7f5470ecece58
lnapalm99 <36113488+lnapalm99@users.noreply.github.com>
2023-03-07 21:55:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+var estado1=true
+var estado2=true
+var estado3=true
+
+function toggleContact() {
+  
+  if (
+    estado1
+    )
+    {
+      document.getElementById("info-contact").style = "display: block";
+      
+    }
+    else 
+    
+    {
+      document.getElementById("info-contact").style = "display: none";
+      
+    }
+    estado1=!estado1
+  }
+  
+  function toggleExperience(){
+    
+    if (
+      estado2
+      )
+      {
+        document.getElementById("experience").style = "display: block";
+        
+      }
+      else 
+      
+      {
+        document.getElementById("experience").style = "display: none";
+        
+      }
+      estado2=!estado2
+    }
+    
+    function toggleKnowledge(){
+      if (
+        estado3
+        )
+        {
+          document.getElementById("knowledge").style = "display: block";
+          
+        }
+        else 
+        
+        {
+          document.getElementById("knowledge").style = "display: none";
+          
+        }
+        estado3=!estado3
+        
+        
+        
+      }
+      
+      
